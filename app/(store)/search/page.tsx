fcc41a6a93d4518242d8335eabcdc2b19d5440e5
Irfan Hussain <I.hussain@expressmedia.int>
2025-01-01 16:11:49 +0500

    add 6th run

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,42 +1,42 @@
 import ProductGrid from "@/components/ProductsGrid";
 import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
 
-type SearchParams = Record<string, string | undefined>;
 
 async function SearchPage({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: SearchParams;
+    searchParams:{
+        query: string;
+    };
 }) {
-  const query = searchParams.query || ""; // Default to an empty string if query is undefined
-
-  const products = await searchProductsByName(query);
-
-  if (!products.length) {
-    return (
-      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
-        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
-          <h1 className="text-3xl font-bold mb-6 text-center">
-            No products found for: {query}
-          </h1>
-          <p className="text-gray-600 text-center">
-            Try searching with different keywords.
-          </p>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
-      <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
-        <h1 className="text-3xl font-bold mb-6 text-center">
-          Search results for {query}
-        </h1>
-        <ProductGrid products={products} />
-      </div>
-    </div>
+    const { query } = await searchParams;
+    const products = await searchProductsByName(query);
+    if (!products.length) {
+          return (
+            <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+                <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+                    <h1 className="text-3xl font-bold mb-6 text-center">
+                    No products found for: {query}
+                    </h1>
+                    <p className="text-gray-600 text-center">
+                    Try searching with different keywords
+                    </p>
+                </div>
+            </div>
+    
   );
 }
+return <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+    <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+        <h1 className="text-3xl font-bold mb-6 text-center">
+        Search result for {query}
+        </h1>
+        <ProductGrid products={products} />
+        </div>
+    </div>
+    
+    
+    
+}
 
-export default SearchPage;
+export default SearchPage
